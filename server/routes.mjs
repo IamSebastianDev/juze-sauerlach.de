1f861a0ea380f1de0b07e91f4acf517db918ab8f
Sebastian <sebastian@iamsebastian.dev>
2022-08-28 14:00:04 +0200

    feat: implemented server structure update

---
+++ server/routes.mjs
@@ -0,0 +1,54 @@
+/** @format */
+
+import { fromRoot } from './utils/fromRoot.util.mjs';
+import express from 'express';
+import { fileService } from './api/services/file.service.mjs';
+import { mailService } from './api/services/mail.service.mjs';
+import { userService } from './api/services/user.service.mjs';
+import { authService } from './api/services/auth.service.mjs';
+import { contentService } from './api/services/content.service.mjs';
+
+export const router = express.Router();
+
+router
+    // app routes
+    .post('/login', () => authService.login())
+    .get('/logout', (req, res) => authService.logout(req, res))
+    .get('/dashboard', authService.isAuthenticated, (req, res) =>
+        res.sendStatus(200).sendFile(fromRoot('./public/dashboard.html'))
+    )
+    .get('/admin', authService.isNotAuthenticated, (req, res) =>
+        res.sendStatus(200).sendFile(fromRoot('./public/admin.html'))
+    )
+    // mail routes
+    .post('/api/v1/mail', async (req, res) => await mailService.send(req, res))
+    // user routes
+    .get('/api/v1/user/:id', async (req, res) => await userService.get(req, res))
+    .post('/api/v1/user', async (req, res) => await userService.post(req, res))
+    .put('/api/v1/user', async (req, res) => await userService.put(req, res))
+    .delete('/api/v1/user', async (req, res) => await userService.delete(req, res))
+    // file routes
+    .get('/api/v1/files', authService.isAuthenticated, async (req, res) => await fileService.get(req, res))
+    .post('/api/v1/files', authService.isAuthenticated, async (req, res) => await fileService.post(req, res))
+    .put('/api/v1/files', authService.isAuthenticated, async (req, res) => await fileService.put(req, res))
+    .delete('/api/v1/files', authService.isAuthenticated, async (req, res) => await fileService.delete(req, res))
+    // content routes
+    .get('/api/v1/page', async (req, res) => await contentService.getAllPages(req, res))
+    .get('/api/v1/page/:id', async (req, res) => await contentService.getPage(req, res))
+    .post('/api/v1/page', authService.isAuthenticated, async (req, res) => await contentService.postPage(req, res))
+    .put('/api/v1/page', authService.isAuthenticated, async (req, res) => await contentService.putPage(req, res))
+    .delete(
+        '/api/v1/page/:id',
+        authService.isAuthenticated,
+        async (req, res) => await contentService.deletePage(req, res)
+    )
+    .get(
+        '/api/v1/page/:id/content',
+        authService.isAuthenticated,
+        async (req, res) => await contentService.getContent(req, res)
+    )
+    .post(
+        '/api/v1/page/:id/content',
+        authService.isAuthenticated,
+        async (req, res) => await contentService.postContent(req, res)
+    );
---
+++ server/server.mjs
@@ -0,0 +1,30 @@
+/** @format */
+
+import '../.config/dotenv.config.mjs';
+import express from 'express';
+import { session } from './middleware/session.middleware.mjs';
+import fileUpload from 'express-fileupload';
+import helmet from 'helmet';
+import compression from 'compression';
+import { router } from './routes.mjs';
+import { fromRoot } from './utils/fromRoot.util.mjs';
+import { authService } from './api/services/auth.service.mjs';
+import { blacklist } from './middleware/blacklist.middleware.mjs';
+
+const app = express();
+[
+    express.json(),
+    fileUpload(),
+    express.urlencoded({ extended: true }),
+    helmet(),
+    blacklist({ blacklist: ['/src/'], matchFull: false }),
+    compression(),
+    session(),
+    authService.initialize(),
+    authService.session(),
+    router,
+    express.static(fromRoot('./site/public/')),
+].forEach((w) => app.use(w));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
