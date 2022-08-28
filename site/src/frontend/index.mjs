521f758759b1f170f4a251dabe05429af2298521
Sebastian <sebastian@iamsebastian.dev>
2022-08-28 23:20:34 +0200

    feat: added webcomponents to frontend bundle

--- site/src/frontend/index.mjs
+++ site/src/frontend/index.mjs
@@ -1,1 +1,12 @@
 /** @format */
+
+import '../shared/components/privacy.mjs';
+import '../shared/components/footer.mjs';
+import '../shared/components/spinner.mjs';
+
+window.addEventListener('submit', (ev) => {
+    if (ev.target.closest('form#login')) {
+        const spinner = document.querySelector('web-spinner');
+        spinner.setAttribute('state', 'true');
+    }
+});
