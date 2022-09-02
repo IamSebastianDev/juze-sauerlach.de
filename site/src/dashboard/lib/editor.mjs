9b82cff553bbf9770ffbf40b9e060310eacf74c2
Sebastian <sebastian@iamsebastian.dev>
2022-09-02 18:29:35 +0200

    feat: added text alignment tool to editor

---
+++ site/src/dashboard/lib/editor.mjs
@@ -0,0 +1,53 @@
+/** @format */
+
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import Paragraph from '@editorjs/paragraph';
+import List from '@editorjs/list';
+import Quote from '@editorjs/quote';
+import Marker from '@editorjs/marker';
+import AlignmentTool from 'editorjs-text-alignment-blocktune';
+import { Delimiter } from '../../shared/editor/tools/delimitor/delimitor.tool.mjs';
+import { Contact } from '../../shared/editor/tools/contact/contact.tool.mjs';
+import { Image } from '../../shared/editor/tools/image/image.tool.mjs';
+
+export const editor = new EditorJS({
+    holder: 'editor',
+    tools: {
+        header: {
+            class: Header,
+            tunes: ['textAlign', 'hyperlink'],
+        },
+        paragraph: {
+            class: Paragraph,
+            inlineToolbar: true,
+            tunes: ['textAlign'],
+        },
+        list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+                defaultStyle: 'unordered',
+            },
+        },
+        quote: Quote,
+        delimiter: Delimiter,
+        contact: Contact,
+        image: {
+            class: Image,
+            inlineToolbar: true,
+            tunes: ['textAlign'],
+        },
+        Marker: {
+            class: Marker,
+            shortcut: 'CMD+SHIFT+M',
+        },
+        textAlign: {
+            class: AlignmentTool,
+            config: {
+                default: 'left',
+            },
+        },
+    },
+    data: JSON.parse(window.localStorage.getItem('editor-temp')) || undefined,
+});
