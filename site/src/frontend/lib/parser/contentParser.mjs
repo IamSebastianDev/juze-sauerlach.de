88705cd9c05790508384d719c06bafe79302712a
Sebastian <sebastian@iamsebastian.dev>
2022-09-03 18:04:07 +0200

    feat: implement content parser

---
+++ site/src/frontend/lib/parser/contentParser.mjs
@@ -0,0 +1,16 @@
+/** @format */
+
+export const contentParser = (dictionary, target) => (content) => {
+    if (!target) return;
+    const { blocks } = content.data;
+
+    const elements = blocks.map(({ id, type, data, tunes }) => {
+        const element = document.createElement(dictionary[type]);
+        element.id = id;
+        element.setAttribute('data', JSON.stringify({ ...data, ...tunes }));
+        return element;
+    });
+
+    [...target.childNodes].forEach((node) => node.remove());
+    target.append(...elements);
+};
