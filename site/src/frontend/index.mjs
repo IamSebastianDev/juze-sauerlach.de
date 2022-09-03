6b29d7df3ff04a464849c271a46893fa501e32c3
Sebastian <sebastian@iamsebastian.dev>
2022-09-03 18:04:34 +0200

    feat: implement page initialization in frontend

--- site/src/frontend/index.mjs
+++ site/src/frontend/index.mjs
@@ -1,12 +1,18 @@
 /** @format */
 
-import '../shared/components/privacy/privacy.mjs';
-import '../shared/components/footer/footer.mjs';
-import '../shared/components/spinner/spinner.mjs';
+import './lib/components.loader.mjs';
+import { page } from '../shared/controllers/page.controller.mjs';
+import { dispatch } from 'straemjs';
+import Røut from '../shared/røut.mjs';
+import { routerConfig } from '../shared/configs/router.config.mjs';
+import { contentParser } from './lib/parser/contentParser.mjs';
+import { elementDictionary } from './lib/parser/elementDictionary.mjs';
 
-window.addEventListener('submit', (ev) => {
-    if (ev.target.closest('form#login')) {
-        const spinner = document.querySelector('web-spinner');
-        spinner.setAttribute('state', 'true');
-    }
+window.addEventListener('DOMContentLoaded', async () => {
+    await page.init();
+    const routes = page.getRoutes(contentParser(elementDictionary, document.querySelector('#content')));
+    const router = new Røut(routes, routerConfig);
+    router.goTo('#home');
+
+    dispatch('page-complete').from(window).with({});
 });
