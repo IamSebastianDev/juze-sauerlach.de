8a72933c88585a2ba7645cf72dcada2fc5012ee1
Sebastian <sebastian@iamsebastian.dev>
2022-09-10 10:43:19 +0200

    fix: reenable rendering of empty pages

--- web/src/dashboard/lib/injectors/injectEditorData.mjs
+++ web/src/dashboard/lib/injectors/injectEditorData.mjs
@@ -2,6 +2,12 @@
 
 export const injectEditorData = (editor) => (content, route, query) => {
     console.log({ content });
-    if (!content || !content.blocks || content.blocks.length === 0) return;
+    if (!content || !content.blocks) return;
+
+    if (content.blocks.length === 0) {
+        editor.blocks.render({ blocks: [] });
+        return;
+    }
+
     editor.blocks.render(content);
 };
